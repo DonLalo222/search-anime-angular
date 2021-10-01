63ee58477986bdc12f34b031bfebcda8ac96153c
Eduardo Gomez <g.vasquez.eduardo@gmail.com>
2021-10-01 16:57:37 -0300

    ghpage

--- src/app/modules/anime/pages/search/search.component.ts
+++ src/app/modules/anime/pages/search/search.component.ts
@@ -4,34 +4,38 @@ import { AnimeService } from '../../services/anime.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
   inputUser: string = '';
   result: any | undefined;
   isLoading: boolean = false;
-  constructor(private animeService: AnimeService) { }
+  constructor(private animeService: AnimeService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async getData(inputUser: string) {
+    if (inputUser.trim() != '') {
+      this.inputUser = inputUser.trim();
+      this.result = undefined;
+      this.isLoading = true;
+      await this.animeService
+        .search(this.inputUser)
+        .then((response) => (this.result = response));
+      this.isLoading = false;
+    }
   }
 
-  async getData(inputUser: string ){
-    this.inputUser = inputUser;
+  async getDataLink(linkUser: string) {
     this.result = undefined;
     this.isLoading = true;
-   await this.animeService.search(this.inputUser).then(response => this.result = response);
-   this.isLoading = false;
-  }
-
-  async getDataLink(linkUser: string){
-    this.result = undefined;
-    this.isLoading = true;
-    await this.animeService.linkToPage(linkUser).then(response => this.result = response);
+    await this.animeService
+      .linkToPage(linkUser)
+      .then((response) => (this.result = response));
     this.isLoading = false;
   }
 
-  async eventKeyup(event: any){
+  async eventKeyup(event: any) {
     await this.getData(event.target.value);
   }
-
 }
